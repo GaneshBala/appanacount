f25cfeea75be0f97b3a3ed4889d24ce40d746bb4
Shrikanth <shrikanth_kr@yahoo.com>
2014-03-03 20:28:48 +0530

    initial

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,5 @@
+class ProjectsController < ApplicationController
+def index 
+	render json: {stats: 'jhhggjij'}, status: 420
+end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tnv::Application.config.secret_key_base = '211535b4f98ba5765c614f5c103b74f39acd0b5876d1f2e9a625e546fbd6afa16eb3ad0272c42f602a35d1894a7371676d5b0dc693370fa52e159c04463ce7bb'
